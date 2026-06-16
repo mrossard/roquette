5f91dd87cec53563a9e3cee527aa49544a7f546e
mrossard <manuel.rossard@u-bordeaux.fr>
2026-06-16 18:41:30 +0200

    fix: auto adding users to todo subchannels feels awkward

--- src/Controller/SubChannelController.php
+++ src/Controller/SubChannelController.php
@@ -108,9 +108,11 @@ final class SubChannelController extends AbstractController
         if ($isTodoList) {
             $channel->setIsTodoList(true);
         }
-
-        foreach ($parentChannel->getMembers() as $member) {
-            $channel->addMember($member);
+        else {
+            // only auto-add members on non "todolist" subchannels
+            foreach ($parentChannel->getMembers() as $member) {
+                $channel->addMember($member);
+            }
         }
 
         $entityManager->persist($channel);
